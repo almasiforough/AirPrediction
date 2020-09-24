9feadd8e86cd4bba210ba8d99cb7edda35b93a40
Forough Almasi <almasi.forough@gmail.com>
2020-09-25 00:36:19 +1000

    deleting src/component/index.js

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ThemeProvider } from "@material-ui/core";
-import Theme from "../../theme/theme";
-
-export default function index({ children }) {
-  return <ThemeProvider theme={Theme}>{children}</ThemeProvider>;
-}
